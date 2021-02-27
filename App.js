08f61767bfe73d986de024a9f7678b3d7ec59e07
Malcolm Miller <malcmill99@gmail.com>
2021-02-27 09:48:48 -0500

    App.js edits
    Attempting to add functionality the the home page

--- App.js
+++ App.js
@@ -1,13 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, SafeAreaView, Button, Alert } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text>Hello Business Students!</Text>
+      <Button 
+        color="orange"
+        title="Find Resources" 
+        onPress={() =>
+          alert.alert("Welcome to BizBuzz","What are your looking for?", [
+            {text: "Find Links", onPress: () => console.log("Business Links")},
+            {text: "Contact info", onPress: () => console.log("business@gmail.com")},
+          ])
+        }
+      />
+{/*      
+        <image source={require("./assets/icon.png")}/> 
+<StatusBar style="auto" /> */}
+    </SafeAreaView>
   );
 }
 
