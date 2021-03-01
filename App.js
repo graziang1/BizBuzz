b3f911930c6ec26d05c7beddc39a129d126c30ad
Gianna <graziang1@newpaltz.edu>
2021-03-01 16:22:11 -0500

    added 2nd button for social media connections

--- App.js
+++ App.js
@@ -16,6 +16,16 @@ export default function App() {
           ])
         }
       />
+      <Button 
+        color="blue"
+        title="Connect With Us" 
+        onPress={() =>
+          alert.alert("Welcome to BizBuzz","What are your looking for?", [
+            {text: "Find Links", onPress: () => console.log("Business Links")},
+            {text: "Contact info", onPress: () => console.log("business@gmail.com")},
+          ])
+        }
+      />
 {/*      
         <image source={require("./assets/icon.png")}/> 
 <StatusBar style="auto" /> */}
