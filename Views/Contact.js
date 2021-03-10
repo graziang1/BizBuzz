ee293b5d3e64162f06a5b9c513a431b8dce20697
Malcolm Miller <malcmill99@gmail.com>
2021-03-10 11:27:50 -0500

    Updated contact page

---
+++ Views/Contact.js
@@ -0,0 +1,11 @@
+<View>
+    <text>Contact Us</text>
+    <text>Facebooks: </text> 
+    <a href= "https://www.facebook.com/SUNYNewPaltzSoB"></a>
+    <text>Instagram: </text>
+    <a href= "www.instagram/npbusiness"></a>
+    <text>LinkedIn: </text>
+    <a href= "https://www.linkedin.com/company/12634419/admin/"></a>
+    <text>Class groups with CircldIn: </text>
+    <a href=  "https://app.circleinapp.com/"></a>
+</View>
