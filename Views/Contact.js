71589e08c82b442801dc42ad8bd5c1ffb3c8c96b
Malcolm Miller <malcmill99@gmail.com>
2021-03-10 11:58:20 -0500

    Contact and Resource updates

--- Views/Contact.js
+++ Views/Contact.js
@@ -1,11 +1,11 @@
 <View>
     <text>Contact Us</text>
-    <text>Facebooks: </text> 
+    <text>Facebook: </text> 
     <a href= "https://www.facebook.com/SUNYNewPaltzSoB"></a>
     <text>Instagram: </text>
     <a href= "www.instagram/npbusiness"></a>
     <text>LinkedIn: </text>
     <a href= "https://www.linkedin.com/company/12634419/admin/"></a>
-    <text>Class groups with CircldIn: </text>
+    <text>Class groups with CircleIn: </text>
     <a href=  "https://app.circleinapp.com/"></a>
 </View>
--- Views/Resources.js
+++ Views/Resources.js
@@ -1,22 +1,41 @@
-<view>
+<View>
+    <text>Resources</text>
+    <text>Events Tab: </text> 
+    <a href= "https://newpaltz.campuslabs.com/engage/events"></a>
+    <text>Networking: </text>
+    <a href= "www.newpaltz.edu/careers/"></a>
+    <text>Workshops: </text>
+    <a href= "www.newpaltz.edu/careers/"></a>
+    <text>Internship/Job Application: </text>
+    <a href=  "www.newpaltz.edu/careers/"></a>
+    <text>Club Announcements: </text>
+    <a href=  "https://newpaltz.campuslabs.com/engage/organizations"></a>
+    <text>Academic Resources: </text>
+    <a href=  "www.newpaltz.edu/schoolofbusiness"></a>
+</View>
+
+{/*
+    var SVGViewElement:{
     <text>Resources</text>
     <text>First, log in to engage!</text>
     <text>Events Tab </text>
-    a.href = https://newpaltz.campuslabs.com/engage/events
+    <a href = https://newpaltz.campuslabs.com/engage/events></a>
     <text>Networking </text>
-    a.href = www.newpaltz.edu/careers/
+    <a href = www.newpaltz.edu/careers/></a>
     <text>Workshops</text>
-    a.href = www.newpaltz.edu/careers/
-    <text>Internship/Job Preparation</text>
-    a.href = www.newpaltz.edu/careers/
+    <a href = www.newpaltz.edu/careers/></a>
+    <text>Internship/Job Preparation </text>
+    <a href = www.newpaltz.edu/careers/></a>
     <text>Club Announcements </text>
-    a.href = https://newpaltz.campuslabs.com/engage/organizations
+    <a href = https://newpaltz.campuslabs.com/engage/organizations
     <text></text>
-    a.href = 
+    <a href = 
     <text></text>
-    a.href = 
+    <a href = 
     <text></text>
-    a.href = 
+    <a href = 
     <text></text>
-    a.href = 
-</view>
+    <a href = 
+</SVGViewElement
+}
+*/}
