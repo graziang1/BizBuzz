df9ff622ee631fa24f3250ac8b4e3040d6fed208
Malcolm Miller <malcmill99@gmail.com>
2021-03-17 11:56:50 -0400

    Info.js stylesheet

--- components/Info.js
+++ components/Info.js
@@ -6,7 +6,7 @@ const Info = (props) => {
     return (
         <View style={styles.item}>
             <View style={styles.itemLeft}>
-                <TouchableOpacity style={styles.square}></TouchableOpacity>
+                <View style={styles.square}></View>
                 <Text style={styles.itemText}>{props.text}</Text>
             </View>
             <View style={styles.circular}></View>
@@ -15,7 +15,38 @@ const Info = (props) => {
 }
 
 const styles = StyleSheet.create({
-
+    item: {
+        backgroundColor: '#FFF',
+        padding: 15,
+        borderRadius:10,
+        flexDirection: 'row',
+        aLignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 20
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        aLignItems: 'center',
+        flexWrap: 'wrap'
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#55BCF6',
+        opacity: 0.4,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemText: {
+        maxWidth: '80%',
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: '#55BCF6',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
 });
 
 export default Info;
